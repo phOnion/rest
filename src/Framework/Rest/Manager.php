97716f4a4210789137176dd49d1fa7d136d57a77
Dimitar Dimitrov <daghostman.dd@gmail.com>
2017-02-28 00:11:03 +0200

    Removed cache and switched to DI for providing supported responses

--- src/Framework/Rest/Manager.php
+++ src/Framework/Rest/Manager.php
@@ -6,12 +6,9 @@ use Onion\Framework\Hydrator\Interfaces\HydratableInterface as Hydratable;
 use Onion\Framework\Rest\Interfaces\SerializerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\SimpleCache\CacheInterface;
 
 class Manager
 {
-    const CACHE_KEY = '__onion_rest_serializers';
-
     /**
      * The transformer object to with hydratable entities
      * @var Transformer
@@ -22,36 +19,14 @@ class Manager
      * A key => value list of namespace => directory
      * to check for available serializers
      *
-     * @var array List of namespaces
+     * @var SerializerInterface[] List of namespaces
      */
-    private $namespace = [];
+    private $serializers = [];
 
-    /**
-     * A cache to store the identified
-     * @var CacheInterface
-     */
-    private $cache;
-
-    public function __construct(Transformer $transformer, array $namespaces = [], CacheInterface $cache = null)
+    public function __construct(Transformer $transformer, array $serializers = [])
     {
         $this->transformer = $transformer;
-        $this->namespace = $namespaces;
-        $this->cache = $cache;
-    }
-
-    private function hasCache(): bool
-    {
-        return $this->cache !== null;
-    }
-
-    private function getCache(): CacheInterface
-    {
-        return $this->cache;
-    }
-
-    public function addNamespace(string $ns, string $directory)
-    {
-        $this->namespace[$ns] = $directory;
+        $this->serializers = $serializers;
     }
 
     /**
@@ -76,31 +51,7 @@ class Manager
 
     public function response(Request $request, Response $response, Hydratable $entity): Response
     {
-        if (!$this->hasCache() || !$this->getCache()->has(self::CACHE_KEY)) {
-            $serializers = [];
-            foreach ($this->namespace as $ns => $path) {
-                $iterator = new \DirectoryIterator($path);
-                while ($iterator->valid()) {
-                    if (!$iterator->isDot() && !$iterator->isDir()) {
-                        $class = $ns . '\\' . substr($iterator->getFilename(), 0, -4);
-                        if (class_exists($class) && in_array(SerializerInterface::class, class_implements($class), true)) {
-                            /** @var $object SerializerInterface */
-                            $serializers[] = new $class;
-                        }
-                    }
-
-                    $iterator->next();
-                }
-            }
-
-            if (!$serializers === [] && $this->hasCache()) {
-                $this->getCache()->set(self::CACHE_KEY, $serializers, 86400);
-            }
-        } else {
-            $serializers = $this->cache->get(self::CACHE_KEY);
-        }
-
-        $serializer = $this->negotiateSerializer(new Accept($request->getHeaderLine('accept')), $serializers);
+        $serializer = $this->negotiateSerializer(new Accept($request->getHeaderLine('accept')), $this->serializers);
         $payload = $this->transformer->transform(
             $entity,
             array_map(function ($value) {
