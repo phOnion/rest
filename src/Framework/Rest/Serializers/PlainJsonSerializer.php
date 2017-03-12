0097f3746f94d3d5a6836a762894cfca41c9177e
Dimitar Dimitrov <daghostman.dd@gmail.com>
2017-03-12 17:56:56 +0200

    Improved handling of 'data' and make aware of root node

--- src/Framework/Rest/Serializers/PlainJsonSerializer.php
+++ src/Framework/Rest/Serializers/PlainJsonSerializer.php
@@ -2,6 +2,7 @@
 namespace Onion\Framework\Rest\Serializers;
 
 use Onion\Framework\Http\Header\Interfaces\AcceptInterface as Accept;
+use Onion\Framework\Hydrator\Interfaces\HydratableInterface;
 use Onion\Framework\Rest\Interfaces\EntityInterface as Entity;
 use Onion\Framework\Rest\Interfaces\SerializerInterface;
 
@@ -18,27 +19,43 @@ class PlainJsonSerializer implements SerializerInterface
             $accept->supports('text/json');
     }
 
-    protected function convert(Entity $entity): array
+    protected function convert(Entity $entity, bool $isRoot = false): array
     {
-        $data = $entity->getData();
+        $data = array_map(function ($entity) {
+            if (is_array($entity)) {
+                foreach ($entity as $index => $item) {
+                    if ($item instanceof Entity) {
+                        $entity[$index] = $this->convert($item);
+                    }
+                }
+            }
+
+            if ($entity instanceof Entity) {
+                $entity = $this->convert($entity);
+            }
+            return $entity;
+        }, $entity->getData());
         foreach ($entity->getEmbedded() as $rel => $relation) {
+
             if (!isset($data[$rel])) {
+
                 if (is_array($relation)) {
                     $payload[$rel] = array_map([$this, 'convert'], $relation);
-                    continue;
                 }
 
-                $data[$rel] = $this->convert($relation);
+                if ($relation instanceof HydratableInterface) {
+                    $data[$rel] = $this->convert($relation);
+                }
             }
         }
 
         return $data;
     }
 
-    public function serialize(Entity $entity): string
+    public function serialize(Entity $entity, bool $isRoot = false): string
     {
         return json_encode(
-            $this->convert($entity),
+            $this->convert($entity, $isRoot),
             JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT
         );
     }
