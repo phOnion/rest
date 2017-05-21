44ae537ed4c588459cc63ecb1480c18b8d18e2cf
Dimitar Dimitrov <daghostman.dd@gmail.com>
2017-05-21 15:54:36 +0300

    Deprecate obsolete interface

--- src/Framework/Rest/Interfaces/SerializableInterface.php
+++ src/Framework/Rest/Interfaces/SerializableInterface.php
@@ -4,6 +4,13 @@ namespace Onion\Framework\Rest\Interfaces;
 
 use Onion\Framework\Hydrator\Interfaces\HydratableInterface as Hydratable;
 
+/**
+ * @deprecated
+ *
+ * Interface SerializableInterface
+ *
+ * @package Onion\Framework\Rest\Interfaces
+ */
 interface SerializableInterface extends Hydratable
 {
     public function getMappings(): array;
